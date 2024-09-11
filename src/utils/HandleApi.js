76a203e0507128896518978fbe5013c81a0b5b7d
git-apna-git <166308688+git-apna-git@users.noreply.github.com>
2024-09-11 16:02:37 +0530

    Update HandleApi.js

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
  
-const baseUrl = "http://localhost:5000"
+const baseUrl = "https://fullstack-todo-app-yt-backend-233n.onrender.com/"
 
 const getAllToDo = (setToDo) => {
     axios
